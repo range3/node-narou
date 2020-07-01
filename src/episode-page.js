0d81118b8d0662b80b4764392eefddd41c7d8ecf
range3 <white.line.oar@gmail.com>
2020-07-01 09:07:07 +0000

    Fix EpisodePage.scrape()

--- src/episode-page.js
+++ src/episode-page.js
@@ -6,9 +6,13 @@ class EpisodePage {
     const $novelContents = $('#novel_contents')
 
     const subtitle = $novelContents.find('.novel_subtitle').text().trim()
-    const preface = $novelContents.find('#novel_p').text().trim()
-    const content = $novelContents.find('#novel_honbun').text().trim()
-    const afterword = $novelContents.find('#novel_a').text().trim()
+    const preface = $novelContents.find('#novel_p p').text().trim()
+    const content = $novelContents
+      .find('#novel_honbun p')
+      .map((i, el) => cheerio(el).text())
+      .get()
+      .join('\n')
+    const afterword = $novelContents.find('#novel_a p').text().trim()
 
     return {
       subtitle,
