2a58dd53813c241050d6ae588bed0e50e9df079d
range3 <white.line.oar@gmail.com>
2021-02-12 06:15:57 +0000

    Update Narou#getNovels: sleep between Narou API call

--- src/narou.js
+++ src/narou.js
@@ -1,31 +1,41 @@
 const Novel = require('./novel')
 const naroujs = require('naroujs')
 
+const MaxBulkGetSize = 100
+
 class Narou {
   novel (ncode) {
     return new Novel(ncode)
   }
 
-  async getNovels (ncodes) {
+  async getNovels (ncodes, { interval } = { interval: 1000 }) {
     if (!ncodes?.length) {
       return []
     }
 
     const novels = []
     for (const slicedNcodes of this.sliceNcodes(ncodes)) {
-      const res = await naroujs({ ncode: slicedNcodes.join('-'), lim: 100 })
+      const res = await naroujs({ ncode: slicedNcodes.join('-'), lim: MaxBulkGetSize })
       novels.push(...slicedNcodes.map(ncode => {
         return new Novel(ncode, res.items?.find(item => item?.ncode.toUpperCase() === ncode.toUpperCase()))
       }))
+      if (ncodes > MaxBulkGetSize) {
+        await this._sleep(interval)
+      }
     }
     return novels
   }
 
   sliceNcodes (ncodes) {
-    const maxSize = 100
-    return new Array(Math.ceil(ncodes.length / maxSize))
+    return new Array(Math.ceil(ncodes.length / MaxBulkGetSize))
       .fill()
-      .map((_, i) => ncodes.slice(i * maxSize, (i + 1) * maxSize))
+      .map((_, i) => ncodes.slice(i * MaxBulkGetSize, (i + 1) * MaxBulkGetSize))
+  }
+
+  _sleep (ms) {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, ms)
+    })
   }
 }
 
